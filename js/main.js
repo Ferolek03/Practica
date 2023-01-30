8ac8f54e634e8fe6eba9b71adff2bc2f014fd328
Ferolek03 <melkonyaynsam@gmail.com>
2023-01-30 22:36:45 +0700

    zad 5 - end

--- js/main.js
+++ js/main.js
@@ -12,20 +12,39 @@ let app = new Vue({
         onSale: true,
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inventory: 100,
+        cart: 0,
+       
+         
         
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ]
-    }
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        delToCart() {
+            this.cart -= 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+         }
+         
+     }
+     
 
 
 })
